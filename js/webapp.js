9e80e7003a650c226d9effc829705ba55be450be
Matt Brandt <mbrandt@mozilla.com>
2013-10-07 10:01:06 -0600

    Grab location and pass it to the wu api call.
    Clean up unneeded comments.

--- js/webapp.js
+++ js/webapp.js
@@ -1,26 +1,9 @@
 (function ($) {
-    /*
-        WebActivities:
-
-            configure
-            costcontrol/balance
-            costcontrol/data_usage
-            costcontrol/telephony
-            dial
-            new (type: "websms/sms", "webcontacts/contact") (add-contact, compose-mail?)
-            open
-            pick (type: "image/png" etc)
-            record (capture?)
-            save-bookmark
-            share
-            test
-            view (type: "url" etc. "text/html"?)
-    */
-
   //Setup an onclick event for the submit button
   $("#submit").click(function() {
+      var location = $("#location").val();
     $.ajax({
-      url : "http://api.wunderground.com/api/" + config.wu_api_key + "/geolookup/conditions/q/IA/Cedar_Rapids.json",
+      url : "http://api.wunderground.com/api/" + config.wu_api_key + "/geolookup/conditions/q/" + location + ".json",
       dataType : "jsonp",
       success : function(parsed_json) {
         var location = parsed_json['location']['city'];
