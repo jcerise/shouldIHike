27ca82dad664aef7e7f6021fe27d8ac5777d7ffc
Jeremy Cerise <jcerise06@gmail.com>
2013-10-08 16:37:12 -0600

    Cleaned up the click handler for the .ajax request
    -The click handler was missing a preventDefault call, which was causing it to do strange things (mainly, the ajax call was failing)
    -Added an error handling function the ajax call is something happens to go wrong

--- js/webapp.js
+++ js/webapp.js
@@ -1,7 +1,8 @@
 (function ($) {
   //Setup an onclick event for the submit button
-  $("#submit").click(function() {
-      var location = $("#location").val();
+  $("#submit").click(function(e) {
+    e.preventDefault();
+    var location = $("#location").val();
     $.ajax({
       url : "http://api.wunderground.com/api/" + config.wu_api_key + "/geolookup/conditions/q/" + location + ".json",
       dataType : "jsonp",
@@ -9,6 +10,9 @@
         var location = parsed_json['location']['city'];
         var temp_f = parsed_json['current_observation']['temp_f'];
         alert("Current temperature in " + location + " is: " + temp_f);
+      },
+      error : function(e) {
+        alert('Request failed...');
       }
     });
   })
